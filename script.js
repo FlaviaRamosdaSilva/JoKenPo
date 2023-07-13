4f0dbe13666e1ac51d11361c6d442e1b986575df
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-07-12 22:49:39 -0300

    testando js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const playHuman = (humanChoice) => {
+    console.log (humanChoice)
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randowNumber = Math.floor (Math.random () *3)
+
+
+    return choices = [randowNumber]
+}
+
+const playTheGame = (human, machine) => {
+console.log ('Humano:' + human + 'Máquina: ' + machine)
+}
