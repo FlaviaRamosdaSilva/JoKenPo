3122e724dd0e5128efcf66a7564bae0bdca069c7
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-07-16 22:41:39 -0300

    finalizado_projeto

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const result = document.querySelector(".result")
+const humanScore = document.querySelector("#human-score")
+const machineScore = document.querySelector('#machine-score')
 
+let humanScoreNumber = 0
+let machineScoreNumber = 0
 
 const playHuman = (humanChoice) => {
     
@@ -17,4 +22,16 @@ const playTheGame = (human, machine) => {
 
     console.log ('Humano: ' + human + "maquina: " + machine)
     
+    if (human === machine) {
+        result.innerHTML = "Deu empate"
+    } else if (human === 'paper' && machine === 'rock' || human === 'rock' && machine === 'scissors' || human === 'scissors' && machine === 'paper') {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = "Parabéns, Você Ganhou!"
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Você perdeu!!"
+    }
 }
+
